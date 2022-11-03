6ce19bbaae2cd08ab61ddc1315a8fcdce2f14416
laurenrey <lara.rey3108@gmail.com>
2022-11-03 14:30:23 +0200

    add corr

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -22,7 +22,7 @@ export function App() {
         break;
 
       default:
-        break;
+        return;
     }
   };
 
@@ -53,55 +53,3 @@ export function App() {
     </Wrapper>
   );
 }
-
-////////////////////////////////////////
-// export class App extends Component {
-//   state = {
-//     good: 0,
-//     neutral: 0,
-//     bad: 0,
-//   };
-
-// onLeaveFeedback = option => {
-//   this.setState(prevState => ({
-//     [option]: prevState[option] + 1,
-//   }));
-// };
-
-// countTotalFeedback = () => {
-//   const { good, neutral, bad } = this.state;
-//   return good + neutral + bad;
-// };
-
-// countPositiveFeedbackPercentage = () => {
-//   const { good } = this.state;
-//   return Math.round((good / this.countTotalFeedback()) * 100) || 0;
-// };
-
-// render() {
-//   const { good, neutral, bad } = this.state;
-//   const total = this.countTotalFeedback();
-//   const positivePercentage = this.countPositiveFeedbackPercentage();
-//   const options = Object.keys(this.state);
-
-// return (
-//   <Wrapper>
-//     <Section title="Please leave feedback">
-//       <FeedbackOptions
-//         options={options}
-//         onLeaveFeedback={this.onLeaveFeedback}
-//       />
-//     </Section>
-//     <Section title="Statistics">
-//       <Statistics
-//         good={good}
-//         neutral={neutral}
-//         bad={bad}
-//         total={total}
-//         positivePercentage={positivePercentage}
-//       />
-//     </Section>
-//   </Wrapper>
-// );
-//   }
-// }
